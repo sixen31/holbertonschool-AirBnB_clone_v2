827de560737c72db5c6d41d7884e258c38375efd
alex <6267@holbertonstudents.com>
2023-12-11 06:47:48 -0800

    correct statement supp maj

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -15,9 +15,10 @@ def hello_hbnb():
 def hbnb():
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
-    return 'C' + escape(text).replace('_', ' ')
+    return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
