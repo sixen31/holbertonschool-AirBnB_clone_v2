e1d43ea4b21f149cfa64787aa144865a95406be9
alex <6267@holbertonstudents.com>
2023-12-11 06:56:04 -0800

    3. Python is cool! Write a script that starts a Flask web application: /python/<text>: display “Python ”, followed by the value of the text variable (replace underscore _ symbols with a space The default value of text is “is cool” You must use the option strict_slashes=False in your route definition

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Write script that start flaks web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text='is_cool'):
+    return 'Python ' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
