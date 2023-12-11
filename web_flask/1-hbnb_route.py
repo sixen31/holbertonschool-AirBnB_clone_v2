40cbf8a668a2d10b72ed0f040a37c188547cea89
alex <6267@holbertonstudents.com>
2023-12-11 06:28:51 -0800

    modifi - for /to @app.route('/hbnb', strict_slashes=False)

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,10 +10,9 @@ app = Flask(__name__)
 def hello_hbnb():
     return "Hello HBNB!"
 
-@app.route('-hbnb', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
