1502e0ad159bd04d90c8aec36ae6c6b55f18cb79
alex <6267@holbertonstudents.com>
2023-12-12 02:40:04 -0800

    supp over 80 char

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -39,10 +39,9 @@ def number_template(n):
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
-    if n % 2 == 0:
-        return render_template('6-number_odd_or_even.html', n=n, odd_even='even')
-    else:
-        return render_template('6-number_odd_or_even.html', n=n, odd_even='odd')
+    template = '6-number_odd_or_even.html'
+    odd_even = 'even' if n % 2 == 0 else 'odd'
+    return render_template(template, n=n, odd_even=odd_even)
 
 
 if __name__ == '__main__':
