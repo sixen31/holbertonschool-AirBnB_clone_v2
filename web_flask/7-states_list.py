d3abd3a53055bb674119d300fd9a472a59ec1173
alex <6267@holbertonstudents.com>
2023-12-14 04:54:55 -0800

    Write a script that starts a Flask web application

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python3
-"""Script that start a flask application and list of states"""
+"""
+    Script that start a flask application and list of states.
+"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
 def state_list():
-    """The function `state_list` retrieves a list of state"""
-    states = storage.all("State")
+    """
+    The function `state_list` retrieves a list of states
+    from storage and renders a template with the
+    list of states.
+    :return: the rendered template "7-states_list.html"
+    with the variable "list_states" set to the value
+    of the "states" variable.
+    """
+    states = list(storage.all(State).values())
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def handle_teardown(exc):
-    """The function "handle_teardown" is used to close the storage"""
+    """
+    The function "handle_teardown" is used to close the storage.
+    :param exc: The "exc" parameter is short for "exception"
+    and is used to handle any exceptions that
+    may occur during the teardown process
+    """
     storage.close()
 
 
